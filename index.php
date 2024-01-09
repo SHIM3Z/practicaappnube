ee01d060111c8b8e19b680c1362d975281903149
SHIM3Z <119704751+SHIM3Z@users.noreply.github.com>
2024-01-08 22:54:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
          "</td><td>" . $fila->apellido .
          "</td><td>" . $fila->direccion .
          "</td><td>" . $fila->telefijo .
-         "</td><td>" . $fila->telemovi .
+         "</td><td>" . $fila->telefmovi .
          "</td><td>" . $fila->email .
          "</td></tr>";
        }
